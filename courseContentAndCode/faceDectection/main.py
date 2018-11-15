a5c1e4163203b9ab1d94421c207a27e3cb7a0446
clarence <2263194561@qq.com>
2018-11-15 15:31:20 +0800

    “face detection simple practice”

--- courseContentAndCode/faceDectection/main.py
+++ courseContentAndCode/faceDectection/main.py
@@ -1,4 +1,5 @@
 import os
+import cv2
 import shutil
 import math
 import random
@@ -7,15 +8,99 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
+class Hog_descriptor():
+    def __init__(self, img, cell_size=16, bin_size=8):
+        self.img = img
+        self.img = np.sqrt(img / np.max(img))
+        self.img = img * 255
+        self.cell_size = cell_size
+        self.bin_size = bin_size
+        self.angle_unit = 360 / self.bin_size
+        # assert type(self.bin_size) == int, "bin_size should be integer,"
+        # assert type(self.cell_size) == int, "cell_size should be integer,"
+        # assert type(self.angle_unit) == int, "bin_size should be divisible by 360"
+
+    def extract(self):
+        height, width = self.img.shape
+        gradient_magnitude, gradient_angle = self.global_gradient()
+        gradient_magnitude = abs(gradient_magnitude)
+
+        cell_gradient_vector = np.zeros((int(height / self.cell_size), int(width / self.cell_size), self.bin_size))
+        for i in range(cell_gradient_vector.shape[0]):
+            for j in range(cell_gradient_vector.shape[1]):
+                cell_magnitude = gradient_magnitude[i * self.cell_size:(i + 1) * self.cell_size,
+                                 j * self.cell_size:(j + 1) * self.cell_size]
+                cell_angle = gradient_angle[i * self.cell_size:(i + 1) * self.cell_size,
+                             j * self.cell_size:(j + 1) * self.cell_size]
+                cell_gradient_vector[i][j] = self.cell_gradient(cell_magnitude, cell_angle)
+
+        hog_image = self.render_gradient(np.zeros([height, width]), cell_gradient_vector)
+        hog_vector = []
+        for i in range(cell_gradient_vector.shape[0] - 1):
+            for j in range(cell_gradient_vector.shape[1] - 1):
+                block_vector = []
+                block_vector.extend(cell_gradient_vector[i][j])
+                block_vector.extend(cell_gradient_vector[i][j + 1])
+                block_vector.extend(cell_gradient_vector[i + 1][j])
+                block_vector.extend(cell_gradient_vector[i + 1][j + 1])
+                mag = lambda vector: math.sqrt(sum(i ** 2 for i in vector))
+                magnitude = mag(block_vector)
+                if magnitude != 0:
+                    normalize = lambda block_vector, magnitude: [element / magnitude for element in block_vector]
+                    block_vector = normalize(block_vector, magnitude)
+                hog_vector.append(block_vector)
+        return hog_vector, hog_image
+
+    def global_gradient(self):
+        gradient_values_x = cv2.Sobel(self.img, cv2.CV_64F, 1, 0, ksize=5)
+        gradient_values_y = cv2.Sobel(self.img, cv2.CV_64F, 0, 1, ksize=5)
+        gradient_magnitude = cv2.addWeighted(gradient_values_x, 0.5, gradient_values_y, 0.5, 0)
+        gradient_angle = cv2.phase(gradient_values_x, gradient_values_y, angleInDegrees=True)
+        return gradient_magnitude, gradient_angle
+
+    def cell_gradient(self, cell_magnitude, cell_angle):
+        orientation_centers = [0] * self.bin_size
+        for i in range(cell_magnitude.shape[0]):
+            for j in range(cell_magnitude.shape[1]):
+                gradient_strength = cell_magnitude[i][j]
+                gradient_angle = cell_angle[i][j]
+                min_angle, max_angle, mod = self.get_closest_bins(gradient_angle)
+                orientation_centers[min_angle] += (gradient_strength * (1 - (mod / self.angle_unit)))
+                orientation_centers[max_angle] += (gradient_strength * (mod / self.angle_unit))
+        return orientation_centers
+
+    def get_closest_bins(self, gradient_angle):
+        idx = int(gradient_angle / self.angle_unit)
+        mod = gradient_angle % self.angle_unit
+        return idx, (idx + 1) % self.bin_size, mod
+
+    def render_gradient(self, image, cell_gradient):
+        cell_width = self.cell_size / 2
+        max_mag = np.array(cell_gradient).max()
+        for x in range(cell_gradient.shape[0]):
+            for y in range(cell_gradient.shape[1]):
+                cell_grad = cell_gradient[x][y]
+                cell_grad /= max_mag
+                angle = 0
+                angle_gap = self.angle_unit
+                for magnitude in cell_grad:
+                    angle_radian = math.radians(angle)
+                    x1 = int(x * self.cell_size + magnitude * cell_width * math.cos(angle_radian))
+                    y1 = int(y * self.cell_size + magnitude * cell_width * math.sin(angle_radian))
+                    x2 = int(x * self.cell_size - magnitude * cell_width * math.cos(angle_radian))
+                    y2 = int(y * self.cell_size - magnitude * cell_width * math.sin(angle_radian))
+                    cv2.line(image, (y1, x1), (y2, x2), int(255 * math.sqrt(magnitude)))
+                    angle += angle_gap
+        return image
+
 # move file to trainSet or testSet
-def moveFile(srcfile, dstfile):
-    if not os.path.isfile(srcfile):
-        print("%s not exist!" %(srcfile))
+def moveFile(srcFilePath, dstPath):
+    if not os.path.isfile(srcFilePath):
+        print("%s not exist!" %(srcFilePath))
     else:
-        filepath, filename = os.path.split(dstfile)
-        if not os.path.exists(filepath):
-            os.makedirs(filepath)
-        shutil.move(srcfile, dstfile)
+        if not os.path.exists(dstPath):
+            os.makedirs(dstPath)
+        shutil.move(srcFilePath, dstPath)
 
 
 #  transform bmp to jpg
@@ -38,33 +123,26 @@ def makeSet(proportion):
     :param proportion: user input the proportion of trainSet number
     :return:
     """
-    path = ['testing', 'training']
+    path = ['trainingSet', 'testingSet']
     for each in path:
         if not os.path.exists(each):
             os.mkdir(each)
     src = 'orl_faces\\s'
-    srcfile = ''
-    dstfile = ''
 
     for i in range(40):
         random_list = os.listdir(src + str(i + 1))
+        print("processing the number s{0} directory.....".format(i + 1))
         random.shuffle(random_list)
         k = proportion * 10
-        for filename in random_list[:k]:
-            srcfile = os.path.join(src + str(i + 1), filename)
-            dstfile = path[0]
-            moveFile(srcfile, dstfile)
-            srcfile = os.path.join(dstfile, filename)
-            dstfile = os.path.join(dstfile,)
-            os.rename(dstf )
-
-
-
-
-
-
-
-
+        for j in range(len(random_list)):
+            dstPath = path[0] if j < k else path[1]
+            filename = random_list[j]
+            srcFilePath = os.path.join(src + str(i + 1), filename)
+            moveFile(srcFilePath, dstPath)
+            srcfile = os.path.join(dstPath, filename)
+            dstfile = os.path.join(dstPath, str(10 * (i + 1) + int(filename.split('.')[0])) + '.pgm')
+            os.rename(srcfile, dstfile)
+            print("move samples from directory {0} ----> {1}".format(srcFilePath, dstfile))
 
 # change the gray picture to row vector
 def img2vector(filename):
@@ -72,6 +150,10 @@ def img2vector(filename):
     # print(img.shape)
     # plt.imshow(img)
     # plt.show()
+    # hog = Hog_descriptor(img, cell_size = 4, bin_size = 8)
+    # vector, image = hog.extract()
+    # return vector[0] # 1*32个特征值
+
     return img.reshape(1, -1)
 
 # 读取训练集和数据集
@@ -81,27 +163,26 @@ def readData(trainFilePath, testFilePath):
 
     trainingLen = len(trainingFileList)
     trainLabels = []
-    trainSet = np.zeros((trainingLen, 10304))
+    trainSet = np.zeros((trainingLen, 92 * 112)) # 92 * 112
     for i in np.arange(trainingLen):
         filename = trainingFileList[i]
         classNum = int(filename.split('.')[0])
-        classNum = math.ceil(classNum / 5)
+        classNum = math.ceil(classNum / 10) - 1
         trainLabels.append(classNum)
         trainSet[i] = img2vector(os.path.join(trainFilePath, filename))
 
     testingLen = len(testFileList)
-    testSet = np.zeros((testingLen, 10304))
+    testSet = np.zeros((testingLen, 92 * 112))
     testLabels = []
     for i in np.arange(testingLen):
-        filename = trainingFileList[i]
+        filename = testFileList[i]
         classNum = int(filename.split('.')[0])
-        classNum = math.ceil(classNum / 5)
+        classNum = math.ceil(classNum / 10) - 1
         testLabels.append(classNum)
         testSet[i] = img2vector(os.path.join(testFilePath, filename))
 
     return {'trainSet' : trainSet, 'trainLabels' : trainLabels,
             'testSet' : testSet, 'testLabels' : testLabels}
-
 
 # 进行归一化处理 normalization
 def maxmin_norm(array):
@@ -131,22 +212,49 @@ def kNNClassify(inX, dataSet, labels, k = 3):
     distance = np.sum(np.power((dataSet - inX), 2), axis = 1) # 计算欧几里得距离
     sortedArray = np.argsort(distance, kind = "quicksort")[:k]
 
-    count = np.zeros(11)
+    # 给距离加入权重
+    w = []
+    for i in range(k):
+        w.append((distance[sortedArray[k-1]] - distance[sortedArray[i]])\
+                 / (distance[sortedArray[k-1]] - distance[sortedArray[0]]))
+
+    count = np.zeros(41)
+    temp = 0
     for each in sortedArray:
-        count[labels[each]] += 1
+        count[labels[each]] += 1 + w[temp]
+        temp += 1
+
     label = np.argmax(count) # 如果label中有多个一样的样本数，那么取第一个最大的样本类别
     return label
 
 def main(k):
+    # generate the trainingSet and the testingSet
+    if not os.path.exists('testingSet') and not os.path.exists("trainingSet"):
+        prompt = "Please input the proportion(0~1): "
+        while True:
+            receive = input(prompt)
+            proportion = round(float(receive), 1)
+            if  0 < proportion < 1:
+                break
+            prompt = "Please input again ! :) :"
+        makeSet(proportion)
+
+    # read data
     data = readData('trainingSet', 'testingSet')
     trainSet = data['trainSet']
     trainLabels = data['trainLabels']
     testSet = data['testSet']
     testLabels = data['testLabels']
 
+    # normalization
+    temp = trainSet.shape[0]
+    array = np.vstack((trainSet, testSet))
+    normalized_array = maxmin_norm(array)
+    trainSet, testSet = normalized_array[:temp], normalized_array[temp:]
+
     correct_count = 0
     test_number = testSet.shape[0]
-    string = "test sample number: {0}, sample label: {1}, classify label: {2}------>correct?: {3}"
+    string = "test sample serial number: {0}, sample label: {1}, classify label: {2}------>correct?: {3}"
     for i in np.arange(test_number):
        label =  kNNClassify(testSet[i], trainSet, trainLabels, k = k)
        if label == testLabels[i]:
@@ -156,18 +264,19 @@ def main(k):
     print("face recognization accuracy: {}%".format((correct_count / test_number) * 100))
     return (correct_count / test_number) * 100
 
+# verify the proper k
 def selectK():
     x = list()
     y = list()
-    for i in range(1, 10):
+    for i in range(3, 11):
         x.append(i)
         y.append(main(i))
     plt.plot(x, y)
     plt.show()
 
-# if __name__ == "__main__":
-#     selectK()
-#
+if __name__ == "__main__":
+   main(4)
+
 # bmpToJpg('orl_faces\\s1')
 # print(img2vector('orl_faces\s1\\1.jpg').shape)
 # deleteImages('orl_faces\s1', 'jpg')
